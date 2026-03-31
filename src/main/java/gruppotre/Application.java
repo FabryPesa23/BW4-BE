2d28d6fd35cc44e2f0ec07c89e9ae418ca6c0158
Andrei Briceag <andrei98briceag@gmail.com>
2026-03-31 11:04:35 +0200

    Main adding database to test

--- src/main/java/gruppotre/Application.java
+++ src/main/java/gruppotre/Application.java
@@ -1,8 +1,18 @@
 package gruppotre;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
 
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("gruppotre");
+        EntityManager em = emf.createEntityManager();
+
         System.out.println("Hello World!");
+
+        em.close();
+        emf.close();
     }
 }
